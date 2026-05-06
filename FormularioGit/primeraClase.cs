b15bc846d04e92a295b9f960b100f8d603028685
Geovaldys Bobadilla <geovaldys.bobadilla@lineadatascan.com>
2026-05-06 17:17:16 -0500

    Se agrega boton 3 para nueva funcionalidad del sistema y primeraClase.class

---
+++ FormularioGit/primeraClase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormularioGit
+{
+    internal class primeraClase
+    {
+        public string usuario { get; set; }
+        public string password { get; set; }
+    }
+}
